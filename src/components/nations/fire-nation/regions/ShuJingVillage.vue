f567c6e985c57bdab8bcc261eb8dee1ef93c94fa
on3dd <onedeadwave@gmail.com>
2020-03-21 15:36:07 +1000

    Added land class to all components

--- src/components/nations/fire-nation/regions/ShuJingVillage.vue
+++ src/components/nations/fire-nation/regions/ShuJingVillage.vue
@@ -1,12 +1,9 @@
 <template>
   <g @mousemove="mouseOver" @mouseleave="mouseLeave">
-    <path
+    <path class="land"
       d="m 1625.7982,1056.5067 24.2792,0.9088 10.7764,19.735 -4.0249,10.6465 -18.4366,13.8924 2.2072,7.5304 -12.8537,5.0636 -9.3482,-12.4642 -10.9062,-8.699 -0.2596,-5.3232 18.4366,-19.4753 z"
       id="shu-jing-village"
-      inkscape:connector-curvature="0">
-      <title
-        id="title8570">Shu Jing Village</title>
-    </path>
+      inkscape:connector-curvature="0"/>
   </g>
 </template>
 
--- src/components/nations/united-republic-of-nations/regions/AirTempleIsland.vue
+++ src/components/nations/united-republic-of-nations/regions/AirTempleIsland.vue
@@ -1,6 +1,6 @@
 <template>
   <g @mousemove="mouseOver" @mouseleave="mouseLeave">
-    <path
+    <path class="land"
       d="m 1637.875,894.5 0.125,9.25 -7.75,13 6.875,10.5 26,-1.375 5.625,-3.5 4.125,6.5 c 0,0 10,2.375 11.125,2.375 1.125,0 11.625,-7.25 11.625,-7.25 l -4.375,-9.5 0.625,-11.875 -9.875,-5.875 -8.625,4.375 -18.125,-11.25 z"
       id="air-temple-island"
       inkscape:connector-curvature="0"/>
